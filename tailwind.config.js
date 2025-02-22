0d4597d3a674fcfb7be34c61a120d6473434dbff
mshr1-create <143092368+mshr1-create@users.noreply.github.com>
2025-02-22 21:04:11 +0900

    setting tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+    "./public/index.html",
+    "./styles/globals.css"
+   ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
